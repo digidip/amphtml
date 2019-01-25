b74a531a2b8efa3b594386673040004ad0bdf4b7
Gil <gil@digidip.net>
2019-01-25 16:47:17 +0100

    add amp-digidip extension

--- bundles.config.js
+++ bundles.config.js
@@ -908,6 +908,11 @@ exports.extensionBundles = [
     latestVersion: '0.1',
     type: TYPES.AD,
   },
+  {
+    name: 'amp-digidip',
+    version: '0.1',
+    type: TYPES.MISC
+  },
 ];
 
 exports.aliasBundles = [
---
+++ extensions/amp-digidip/0.1/amp-digidip.js
@@ -0,0 +1,105 @@
+/**
+ * Copyright 2019 The AMP HTML Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {AmpEvents} from '../../../src/amp-events';
+import {LinkShifter} from './link-shifter';
+import {Services} from '../../../src/services';
+import {getConfigOpts} from './config-options';
+import {getScopeElements} from './helper';
+
+export class AmpDigidip extends AMP.BaseElement {
+
+  /** @param {!AmpElement} element */
+  constructor(element) {
+    super(element);
+
+    /** @private {?../../../src/service/ampdoc-impl.AmpDoc} */
+    this.ampDoc_ = null;
+
+    /** @private {?../../../src/service/viewer-impl.Viewer} */
+    this.viewer_ = null;
+
+    /* @private {?./link-shifter} */
+    this.shifter_ = null;
+
+    /** @private {?Object} */
+    this.configOpts_ = {};
+
+    /** @private {?Object} */
+    this.listElements_ = null;
+
+    /** @private {!Document|!ShadowRoot} */
+    this.rootNode_ = null;
+  }
+
+  /** @override */
+  buildCallback() {
+
+    this.ampDoc_ = this.getAmpDoc();
+    this.rootNode_ = this.ampDoc_.getRootNode();
+    this.viewer_ = Services.viewerForDoc(this.ampDoc_);
+
+    this.configOpts_ = getConfigOpts(this.element);
+
+    return this.ampDoc_.whenBodyAvailable()
+        .then(() => {
+          this.letsRockIt_();
+        });
+  }
+
+  /**
+   * @private
+   */
+  letsRockIt_() {
+    this.shifter_ = new LinkShifter(
+        this.element,
+        this.viewer_,
+        this.ampDoc_);
+
+    this.attachClickEvent_();
+
+    this.rootNode_.addEventListener(AmpEvents.DOM_UPDATE,
+        this.attachClickEvent_());
+  }
+
+  /**
+   * @private
+   */
+  attachClickEvent_() {
+    this.listElements_ = getScopeElements(
+        this.ampDoc_,
+        this.configOpts_);
+
+    this.listElements_.forEach(nodeElement => {
+      nodeElement.addEventListener('click', event => {
+        this.shifter_.clickHandler(event);
+      }, false);
+
+      nodeElement.addEventListener('contextmenu', event => {
+        this.shifter_.clickHandler(event);
+      }, false);
+    });
+  }
+
+  /** @override */
+  isLayoutSupported() {
+    return true;
+  }
+}
+
+AMP.extension('amp-digidip', '0.1', AMP => {
+  AMP.registerElement('amp-digidip', AmpDigidip);
+});
